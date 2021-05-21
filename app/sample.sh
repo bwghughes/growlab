e260722f9d1729c2f06c0f1d79cf2b91f4759bc0
Ben Hughes <bwghughes@gmail.com>
2021-05-21 21:18:00 +0100

    Yah

--- app/sample.sh
+++ app/sample.sh
@@ -12,3 +12,5 @@ git commit -s -m "Update images at `date`"
 git pull origin master --rebase
 git push origin master
 
+s3cmd put $(pwd)/preview.jpg s3://growlab-archive/image-$(date -d "today" +"%Y%m%d%H%M").jpg
+
